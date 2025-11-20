97459029bbf0e628adfe0174cdd81196b67cfec3
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2025-11-20 11:47:15 +0530

    view updated

--- dumbble/views.py
+++ dumbble/views.py
@@ -78,7 +78,7 @@ def product(request):
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    return render(request, "product_details.html", {"product": product})
+    return render(request, "product_detail.html", {"product": product})
 
 
 # def checkout_view(request):
