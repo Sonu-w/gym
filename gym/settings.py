7cad60f06be6c05003711ed1a0577265bd4716e5
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2025-11-20 11:03:55 +0530

    Update settings.py

--- gym/settings.py
+++ gym/settings.py
@@ -126,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+if 'RENDER' in os.environ:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
